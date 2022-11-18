bb6c8a1ea1492e5f6f38234dc49e9d3c80a3fb13
Nguyễn Ngọc Anh <anhnn02.it@gmail.com>
2022-11-19 01:53:24 +0700

    feat: login

---
+++ script.js
@@ -0,0 +1,48 @@
+function progress(){
+  var percent = document.querySelector('.percent');
+  var progress = document.querySelector('.progress');
+  var text = document.querySelector('.text');
+  var loading = document.querySelector('.loading');
+  loading.style.display = 'inline-block'
+  var count = 4;
+  var per = 16;
+  var loading = setInterval(animate, 25);
+  function animate(){
+    if(count == 100 && per == 400){
+      percent.classList.add("text-blink");
+      text.style.display = "inline-block";
+      clearInterval(loading);
+    }else{
+      per = per + 4;
+      count = count + 1;
+      progress.style.width = per + 'px';
+      percent.textContent = count + '%';
+    }
+  }
+}
+
+function myFunction() {
+  const phoneNumber = document.querySelector('#phoneNumber');
+  const code = document.querySelector('#code');
+  const showError = document.querySelector('.show-error');
+
+  if(phoneNumber && code && showError) {
+    if(phoneNumber.value.trim() == "" || code.value.trim() == "") {
+      showError.innerHTML = "<span class='text-danger' >Vui lòng nhập thông tin!</span>";
+      return
+    }
+    if(code.value.trim() === "mpm97") {
+      showError.innerHTML = "<span class='text-danger' >Mã phần mềm này không hỗ trợ trên điện thoại, máy tính của bản. Mời nâng cấp gói phần mềm khác!</span>";
+      return
+    }
+    if(code.value.trim() !== "quang123") {
+      return
+    }
+    progress();
+    setTimeout(function () {
+      window.location.href = "https://zalo.me";
+    }, 2500);
+  }
+  //window.location.href = "fb://page/300881516665819/reviews";
+}
+
